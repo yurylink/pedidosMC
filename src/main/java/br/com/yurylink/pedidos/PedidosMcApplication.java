a2aad500be388746e558b71e97d6089b6416d124
Wesley Almeida <yurylink@hotmail.com>
2018-07-04 20:30:07 -0300

    Criação do projeto

---
+++ src/main/java/br/com/yurylink/pedidos/PedidosMcApplication.java
@@ -0,0 +1,12 @@
+package br.com.yurylink.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosMcApplication.class, args);
+	}
+}
