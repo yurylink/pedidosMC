ddfece9d456ade9e0a8b03e40206e4eeff0b05e4
Wesley Almeida <yurylink@hotmail.com>
2018-07-04 20:44:39 -0300

    Rest Works

---
+++ src/main/java/br/com/yurylink/pedidos/resources/CategoriaResources.java
@@ -0,0 +1,15 @@
+package br.com.yurylink.pedidos.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResources {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest Works !!!";
+	}
+}
