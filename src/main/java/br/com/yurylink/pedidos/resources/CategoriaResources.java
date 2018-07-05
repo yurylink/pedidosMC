96dcd49c7e6d466bbc3fdc4cbbec50e8430dcb1a
Wesley Almeida <yurylink@hotmail.com>
2018-07-05 17:36:40 -0300

    Implementando classe categoria

--- src/main/java/br/com/yurylink/pedidos/resources/CategoriaResources.java
+++ src/main/java/br/com/yurylink/pedidos/resources/CategoriaResources.java
@@ -1,15 +1,28 @@
 package br.com.yurylink.pedidos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.yurylink.pedidos.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest Works !!!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> listaCategoria = new ArrayList<Categoria>();
+		listaCategoria.add(cat1);
+		listaCategoria.add(cat2);
+		
+		return listaCategoria;
 	}
 }
