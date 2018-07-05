0e8c359054297b125087ed7a5b92ec4c2a8f96fc
Wesley Almeida <yurylink@hotmail.com>
2018-07-05 20:25:14 -0300

    Adicionando classe produto

--- src/main/java/br/com/yurylink/pedidos/PedidosMcApplication.java
+++ src/main/java/br/com/yurylink/pedidos/PedidosMcApplication.java
@@ -1,16 +1,17 @@
 package br.com.yurylink.pedidos;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.yurylink.pedidos.domain.Categoria;
+import br.com.yurylink.pedidos.domain.Produto;
+import br.com.yurylink.pedidos.repositories.ProdutoRepository;
 import br.com.yurylink.pedidos.repositories.services.CategoriaServices;
 
 @SpringBootApplication
@@ -18,6 +19,9 @@ public class PedidosMcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaServices categoriaService;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosMcApplication.class, args);
@@ -32,6 +36,19 @@ public class PedidosMcApplication implements CommandLineRunner{
 		listaCategoria.add(cat1);
 		listaCategoria.add(cat2);
 		
+		Produto p1 = new Produto(null, "Notebook", 1200.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 25.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaService.saveAll(listaCategoria);
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
+	
 }
--- src/main/java/br/com/yurylink/pedidos/domain/Categoria.java
+++ src/main/java/br/com/yurylink/pedidos/domain/Categoria.java
@@ -1,11 +1,14 @@
 package br.com.yurylink.pedidos.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -16,6 +19,9 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
 	public Categoria() {
 		
 	}
@@ -24,6 +30,15 @@ public class Categoria implements Serializable{
 		this.nome= nome;
 		this.id = id;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 
 	public Integer getId() {
 		return id;
@@ -65,5 +80,5 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
+
 }
